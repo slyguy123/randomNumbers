3fb4fb6995c1e49742f66424bc00e4bc23f07ec7
slyguy123 <guy.schuleman@gmail.com>
2023-07-03 21:05:40 +0100

    Adding html scrapper to get results

--- randomNumberGenerator.py
+++ randomNumberGenerator.py
@@ -24,3 +24,5 @@ additional_numbers = generate_additional_numbers(2, mySeed)
 print("Random numbers from 1 to 50:", random_numbers)
 print("Additional numbers from 1 to 12:", additional_numbers)
 print("With the seed:", mySeed)
+
+
---
+++ scrapeLottoResults.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Send a GET request to the webpage
+url = "https://www.lottery.co.uk/euromillions/results/past"
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the table containing the results
+results_table = soup.find("table", class_="dataList")
+
+# Initialize a list to store the results
+results_list = []
+
+# Iterate over each row in the table (excluding the header row)
+for row in results_table.find_all("tr")[1:]:
+    # Extract the date and numbers from each row
+    cells = row.find_all("td")
+    date = cells[0].text.strip()
+    numbers = [cell.text.strip() for cell in cells[1:-1]]
+    lucky_stars = [cells[-1].text.strip()]
+
+    # Append the result to the list
+    result = {"Date": date, "Numbers": numbers, "Lucky Stars": lucky_stars}
+    results_list.append(result)
+
+# Save the results to a file
+filename = "euromillions_results.txt"
+with open(filename, "w", encoding="utf-8") as file:
+    for result in results_list:
+        file.write(f"Date: {result['Date']}\n")
+        file.write(f"Numbers: {', '.join(result['Numbers'])}\n")
+        file.write(f"Lucky Stars: {', '.join(result['Lucky Stars'])}\n")
+        file.write("\n")
+
+print(f"Results saved to {filename}")

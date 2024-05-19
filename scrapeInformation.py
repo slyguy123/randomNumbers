4145b178fa46e84df5dba628c512a47bb6e08eda
slyguy123 <guy.schuleman@gmail.com>
2024-05-19 21:57:11 +0100

    Adding colum for lucky stars and payouts

--- scrapeInformation.py
+++ scrapeInformation.py
@@ -21,15 +21,32 @@ def scrape_results(year):
     results_table = soup.find("table", class_="table euromillions mobFormat")
     results_list = []
 
+    #for row in results_table.find_all("tr")[1:]:
+    #    cells = row.find_all("td")
+    #    date = cells[0].text.strip()
+    #    #numbers = [cell.text.strip() for cell in cells[1:-1]]
+    #    numbers = [cells[1].text.strip()]
+    #    lucky_stars = [cells[-1].text.strip()]
+    #    result = {"Date": date, "Numbers": numbers, "Lucky Stars": lucky_stars}
+    #    results_list.append(result)
+
+    # Adding lucky stars and payouts colum. 
+    # TO DO - need to adjust coloum lookups in extract commonValues to match.
+    # also need to check that the commas are supported in the text.
     for row in results_table.find_all("tr")[1:]:
         cells = row.find_all("td")
         date = cells[0].text.strip()
-        #numbers = [cell.text.strip() for cell in cells[1:-1]]
         numbers = [cells[1].text.strip()]
-        lucky_stars = [cells[-1].text.strip()]
-        result = {"Date": date, "Numbers": numbers, "Lucky Stars": lucky_stars}
-        results_list.append(result)
 
+        current_list = numbers[0].split('\n')
+        frontNums = current_list[0:5]
+        #lucky_stars = [cells[-1].text.strip()]
+        lucky_stars = current_list[5:]
+        payouts = [cells[-1].text.strip()]
+        result = {"Date": date, "Numbers": frontNums, "Lucky Stars": lucky_stars, "Payouts": payouts}
+        results_list.append(result)
+        print(frontNums)
+        print(lucky_stars)
     return results_list
 
 # Scrape the EuroMillions results for each year from 2016 to the present
@@ -51,8 +68,8 @@ filename = os.path.join(curDir, fName)
 
 with open(filename, "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["Date", "Numbers", "Lucky Stars"])
+    writer.writerow(["Date", "Numbers", "Lucky Stars", "Payouts"])
     for result in results:
-        writer.writerow([result["Date"], ", ".join(result["Numbers"]), ", ".join(result["Lucky Stars"])])
+        writer.writerow([result["Date"], ", ".join(result["Numbers"]), ", ".join(result["Lucky Stars"]), ", ".join(result["Payouts"])])
 print(f"Results saved to {filename}")
 

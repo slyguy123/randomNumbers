3f49ed23c888e80fb185226e9e070d815dc270d0
slyguy123 <guy.schuleman@gmail.com>
2024-08-01 17:21:34 +0100

    Adding checks for csv file and clean up before you generate a new one.

--- scrapeInformation.py
+++ scrapeInformation.py
@@ -33,6 +33,7 @@ def scrape_results(year):
     # Adding lucky stars and payouts colum. 
     # TO DO - need to adjust coloum lookups in extract commonValues to match.
     # also need to check that the commas are supported in the text.
+    
     for row in results_table.find_all("tr")[1:]:
         cells = row.find_all("td")
         date = cells[0].text.strip()
@@ -45,17 +46,22 @@ def scrape_results(year):
         payouts = [cells[-1].text.strip()]
         result = {"Date": date, "Numbers": frontNums, "Lucky Stars": lucky_stars, "Payouts": payouts}
         results_list.append(result)
-        print(frontNums)
-        print(lucky_stars)
+        #print(frontNums)
+
+        #print(lucky_stars)
     return results_list
+
+def check_and_remove_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"# File {file_path} was already present and has been removed. #")
+    else:
+        print(f"# First time running. File {file_path} does not exist. #")
 
 # Scrape the EuroMillions results for each year from 2016 to the present
 start_year = 2016
 current_year = datetime.datetime.now().year
 results = []
-
-for year in range(start_year, current_year + 1):
-    results.extend(scrape_results(year))
 
 # Save the results to a CSV file
 curDir = os.getcwd()
@@ -64,12 +70,20 @@ curDir = os.getcwd()
 fName = "euromillions_results.csv"
 filename = os.path.join(curDir, fName)
 
+# check for files and clean if needed
+check_and_remove_file(filename)
 
+# run scrape - add cool progress and write to file
+hashes = []
+for year in range(start_year, current_year + 1):
+    results.extend(scrape_results(year))
+    hashes.append("#")
+    print(hashes)
 
 with open(filename, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Date", "Numbers", "Lucky Stars", "Payouts"])
     for result in results:
         writer.writerow([result["Date"], ", ".join(result["Numbers"]), ", ".join(result["Lucky Stars"]), ", ".join(result["Payouts"])])
-print(f"Results saved to {filename}")
+print(f"## New results saved to {filename} ##")
 
